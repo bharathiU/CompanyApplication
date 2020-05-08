0bd285af6b7c9f507722e141f86750ff64fb3470
bharathiU <55991274+bharathiU@users.noreply.github.com>
2020-05-08 12:25:45 +0530

    Update ErrorDetails.java
    update

--- EmployeeApplication2/src/main/java/com/employee/controller/exceptionhandler/ErrorDetails.java
+++ EmployeeApplication2/src/main/java/com/employee/controller/exceptionhandler/ErrorDetails.java
@@ -1,35 +1,26 @@
 package com.employee.controller.exceptionhandler;
-
 import java.time.LocalDateTime;
-
 public class ErrorDetails {
 	private String message;
 	private LocalDateTime timeStamp;
-
 	public String getMessage() {
 		return message;
 	}
-
 	public void setMessage(String message) {
 		this.message = message;
 	}
-
 	public LocalDateTime getTimeStamp() {
 		return timeStamp;
 	}
-
 	public void setTimeStamp(LocalDateTime timeStamp) {
 		this.timeStamp = timeStamp;
 	}
-
 	@Override
 	public String toString() {
 		return "ErrorDetails [message=" + message + ", timeStamp=" + timeStamp + "]";
 	}
-
 	public ErrorDetails(String message, LocalDateTime timeStamp) {
 		this.message = message;
 		this.timeStamp = timeStamp;
 	}
-
 }
